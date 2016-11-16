507e7bf0b10c44928fff7907d6374e27dbde9f31
Youk Chang SU <csyouk@hanmail.net>
2016-11-16 17:21:55 +0900

    selenium으로 웹브라우저를 실행한 후, dom들을 캡처하다.

--- pyselenium/test_selenium.py
+++ pyselenium/test_selenium.py
@@ -1,9 +1,16 @@
 from time import sleep
 from selenium import webdriver
+# from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("http://www.remoteworking.co/#s=2")
 print("done!")
-sleep(2)
+sleep(5)
 company = driver.find_element_by_class_name("company")
 print(company)
+
+companies = driver.find_elements_by_tag_name("a")
+for company in companies:
+    print "text : ", company.text
+# print "companies", companies
+print dir(companies[0])
